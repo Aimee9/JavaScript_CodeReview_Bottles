5019d9f0e3d402a9bbd90d97f1a07d4dc911b286
Aimee <akmreiss@yahoo.com>
2015-09-17 21:11:23 -0700

    Puts user number in a sentence

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@ var bottleAmount = function(number) {
   return number;
 };
 
-
+var completeSentence = function(number) {
+  var whole = number + " bottles of beer on the wall.";
+  return whole;
+}
 
 // $(document).ready(function() {
 //   $("form#factorial").submit(function(event) {
--- spec/specs.js
+++ spec/specs.js
@@ -3,3 +3,9 @@ describe('bottleAmount', function() {
     expect(bottleAmount(5)).to.eql(5);
   });
 });
+
+describe('completeSentence', function() {
+  it("will take user input to complete the sentence", function() {
+    expect(completeSentence(20)).to.eql("20 bottles of beer on the wall.");
+  });
+});
