e22999ac6816bb362c848980b6f15e27c819cf95
oem <oem@oems-MacBook-Pro.local>
2015-09-17 20:49:10 -0700

    Get a user number

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var bottleAmount = function(number) {
+  return number;
+};
+
+//
+//
+// $(document).ready(function() {
+//   $("form#factorial").submit(function(event) {
+//     var number= parseInt($("input#number").val());
+//     var result = factorial(number);
+//
+//       $(".number").text(number)
+//       $(".final").text(result)
+//        $("#result").show()
+//
+//
+//     event.preventDefault();
+//   });
+// });
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('bottleAmount', function() {
+  it("will take a number and output that number", function() {
+    expect(bottleAmount(5)).to.eql(5);
+  });
+});
