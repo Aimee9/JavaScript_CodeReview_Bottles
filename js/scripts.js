6545933d8723448614b583f091ec13c21563d3de
Aimee <akmreiss@yahoo.com>
2015-09-18 12:39:12 -0700

    Decrease user number

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,20 @@ var bottleAmount = function(number) {
 var completeSentence = function(number) {
   var whole = number + " bottles of beer on the wall.";
   return whole;
-}
+};
+
+var lessNumber = function(number) {
+  var printNumber = "";
+  for(var i = number; i > 0; i--) {
+    printNumber += (i);
+    printNumber += (" ");
+ } return printNumber;
+  // while (number > 0) {
+  //   console.log(number);
+  //   return number;
+  //   number--;
+  //     console.log(number);
+};
 
 // $(document).ready(function() {
 //   $("form#factorial").submit(function(event) {
--- spec/specs.js
+++ spec/specs.js
@@ -9,3 +9,9 @@ describe('completeSentence', function() {
     expect(completeSentence(20)).to.eql("20 bottles of beer on the wall.");
   });
 });
+
+describe('lessNumber', function() {
+  it("will decrease the user number by one down to zero", function() {
+    expect(lessNumber(5)).to.eql("5 4 3 2 1 ");
+  });
+});
