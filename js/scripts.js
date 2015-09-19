d563944f889a3c079023dab7642623c14026cfde
Aimee <akmreiss@yahoo.com>
2015-09-18 17:18:22 -0700

    Sing bottle of beer song

--- js/scripts.js
+++ js/scripts.js
@@ -18,24 +18,20 @@ var completeSentence = function(number) {
 
 var bottleCount = function(number) {
   var song = "";
-  while(number > 0){
+  for(var i = number; i >= 0; i--){
     if(number > 2) {
-      song = number + " bottles of beer on the wall, " + number + " bottles of beer.  Take one down, pass it around " + (number - 1) + " bottles of beer on the wall.";
+      song += number + " bottles of beer on the wall, " + number + " bottles of beer.  Take one down, pass it around, " + (number - 1) + " bottles of beer on the wall.  ";
       number--;
-      return song;
     } else if(number === 2) {
-      song = number + " bottles of beer on the wall, " + number + " bottles of beer.  Take one down, pass it around " + (number - 1) + " bottle of beer on the wall.";
-      number--;
-      return song;
+      song += number + " bottles of beer on the wall, " + number + " bottles of beer.  Take one down, pass it around, " + (number - 1) + " bottle of beer on the wall.  ";
+        number--;
     } else if(number === 1) {
-      song = number + " bottle of beer on the wall, " + number + " bottle of beer.  Take one down, pass it around " + (number - 1) + " bottles of beer on the wall.";
-      number--;
-      return song;
+      song += number + " bottle of beer on the wall, " + number + " bottle of beer.  Take one down, pass it around, " + (number - 1) + " bottles of beer on the wall.  ";
+        number--;
     } else {
-      song = "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.";
-      return song;
-    };
-  };
+      song += "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.";
+    }
+  } return song;
 };
 
 $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -18,6 +18,6 @@ describe('completeSentence', function() {
 
 describe('bottleCount', function() {
   it("will decrease the user number by one down to zero with sentences", function() {
-    expect(bottleCount(3)).to.eql("Take one down and pass it around, 1 bottles of beer on the wall.");
+    expect(bottleCount(3)).to.eql("3 bottles of beer on the wall, 3 bottles of beer.  Take one down, pass it around, 2 bottles of beer on the wall.  2 bottles of beer on the wall, 2 bottles of beer.  Take one down, pass it around, 1 bottle of beer on the wall.  1 bottle of beer on the wall, 1 bottle of beer.  Take one down, pass it around, 0 bottles of beer on the wall.  No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
   });
 });
